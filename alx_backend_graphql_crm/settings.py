808f425c1989dbc901e29e4e24fe643ff83fdda4
kelvyn2012 <kudufo@gmail.com>
2025-10-18 07:00:53 +0100

    trying to pass checker

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # installed apps
     'crm',
     'graphene_django',
     'django_filters',
